149f0b93b9d82bf25a77bac0d1ab7694ac3553b3
Derrick Goh <derrickgoh@Derricks-MacBook-Pro.local>
2017-03-21 21:09:53 +0800

    fixed more bugs
    asdfg

--- src/java/dao/DataManager.java
+++ src/java/dao/DataManager.java
@@ -29,7 +29,11 @@ public class DataManager {
     
     public DataManager() throws IOException{
         Gson gson = new Gson();
-        String contents = new String(Files.readAllBytes(Paths.get("./build/web/WEB-INF/classes/config/config.json")));
+        try {
+            String contents = new String(Files.readAllBytes(Paths.get("./build/web/WEB-INF/classes/config/config.json")));
+        } catch (Exception e){
+            new String(Files.readAllBytes(Paths.get("./build/WEB-INF/classes/config/config.json")));
+        }
         cm = gson.fromJson(contents, ConnectionManager.class);
     }
     
